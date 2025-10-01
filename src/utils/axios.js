76594894a754ad489ecd078d3be5f7e80eace5f4
greensalttt <oversensethema@naver.com>
2025-10-01 16:05:40 +0900

    feat: axios 인스턴스 및 인증 토큰 인터셉터

---
+++ src/utils/axios.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    // baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://greensalt.site/api',
+    timeout: 5000,
+})
+
+// 요청 인터셉터: Authorization 헤더 자동 추가
+instance.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('adminToken')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    error => Promise.reject(error)
+)
+
+export default instance
